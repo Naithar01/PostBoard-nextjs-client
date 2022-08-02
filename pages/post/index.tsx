0fd02b3a111906282cf0e139b334838e9347c2e7
Naithar2 <rlawjdgh37@gmail.com>
2022-08-02 09:12:38 +0900

    Post Index 페이지 작성

---
+++ pages/post/index.tsx
@@ -0,0 +1,61 @@
+import { GetServerSideProps } from "next";
+import { Fragment } from "react";
+import PageHeader from "../../components/Layouts/PageHeader/PageHeader";
+import PostItem from "../../components/Post/PostItem";
+
+import styles from "../../styles/post/post.module.css";
+
+export type Post = {
+  id: string;
+  author: string;
+  title: string;
+  content: string;
+  create_at: string;
+};
+
+interface IProps {
+  post: Post[];
+}
+
+export const DUMMY_POST: Post[] = [
+  {
+    id: "11-aa",
+    author: "user1",
+    title: "title1",
+    content: "content1",
+    create_at: "2022-08-02",
+  },
+  {
+    id: "22-bb",
+    author: "user1231",
+    title: "title12131",
+    content: "content123121",
+    create_at: "2022-08-02",
+  },
+];
+
+const PostMainPage = ({ post }: IProps) => {
+  return (
+    <div className="post_main_page">
+      <PageHeader header_text="Posts" />
+      <div className="post_length">
+        <p>[Written Post Count: {post.length}]</p>
+      </div>
+      <div className={styles.post_item_list}>
+        {DUMMY_POST.map((post, index) => (
+          <PostItem post={post} key={post.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {
+      post: DUMMY_POST,
+    },
+  };
+};
+
+export default PostMainPage;
